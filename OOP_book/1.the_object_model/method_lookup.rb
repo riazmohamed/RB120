034aa4149068d29a6de51c6007db6a76dc1d90fc
riazmohamed <railsdevriaz@gmail.com>
2021-08-24 12:23:18 +0100

    add method_lookup.rb

---
+++ OOP_book/1.the_object_model/method_lookup.rb
@@ -0,0 +1,38 @@
+module Speak
+  def speak(sound)
+    puts sound
+  end
+end
+
+class GoodDog
+  include Speak
+end
+
+class HumanBeing
+  include Speak
+end
+
+sparky = GoodDog.new
+bob = HumanBeing.new
+
+puts "------GoodDog ancestors-------"
+puts GoodDog.ancestors
+puts ""
+puts "------HumanBeing ancestors-------"
+puts HumanBeing.ancestors
+
+# ------GoodDog ancestors-------
+# GoodDog
+# Speak
+# Object
+# PP::ObjectMixin
+# Kernel
+# BasicObject
+
+# ------HumanBeing ancestors-------
+# HumanBeing
+# Speak
+# Object
+# PP::ObjectMixin
+# Kernel
+# BasicObject
