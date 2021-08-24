9fa04d71c956ab333a64e7070f2ced47f32f79ef
riazmohamed <railsdevriaz@gmail.com>
2021-08-24 15:17:11 +0100

    ex1.rb

---
+++ OOP_book/1.the_object_model/ex1.rb
@@ -0,0 +1,11 @@
+# How to create an object in Ruby? Give an example of the creation of the object.
+
+# A new instance of a class is created in Ruby by calling the class method #new on the class.
+# This is known as the instantiation of the Object
+
+# Example
+
+class HumanBeing #defining a class
+end
+
+p bob = HumanBeing.new # instantiation of a new object/instance of the HumanBeing class
