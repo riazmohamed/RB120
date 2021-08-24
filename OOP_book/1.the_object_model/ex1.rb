87666b71bbc49876a8640aa6443729a71369bbf0
riazmohamed <railsdevriaz@gmail.com>
2021-08-24 16:06:46 +0100

    add module

--- OOP_book/1.the_object_model/ex1.rb
+++ OOP_book/1.the_object_model/ex1.rb
@@ -4,8 +4,23 @@
 # This is known as the instantiation of the Object
 
 # Example
-
-class HumanBeing #defining a class
+module Sound
+  def speak(sound)
+    puts sound
+  end
 end
 
-p bob = HumanBeing.new # instantiation of a new object/instance of the HumanBeing class
+class HumanBeing #defining a class
+  include Sound
+end
+
+bob = HumanBeing.new # instantiation of a new object/instance of the HumanBeing class
+bob.speak("How are you?")
+
+# What is a module? What is its purpose? How do we use them with our classes? Create a module for the class you created in exercise 1 and include it properly.
+
+# MOdule definition
+  # - In Ruby Mutiple inheritance is acheived by "mixing-in" modules using the include method invocation followed by the module name.
+  # Modules are a collection of classes, methods and constants.
+  # group reusable code in one place
+  # Module are also used as a name space
