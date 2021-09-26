4c0af7551acc16f722684f95e588797056b07aab
riazmohamed <railsdevriaz@gmail.com>
2021-09-26 13:31:19 +0100

    add hard solution

---
+++ Lesson4/Hard/ex1.rb
@@ -0,0 +1,19 @@
+class SecretFile
+  attr_reader :data
+
+  def initialize(secret_data, logger)
+    @data = secret_data
+    @logger = logger
+  end
+
+  def data
+    @logger.create_log_entry
+    @data
+  end
+end
+
+class SecurityLogger
+  def create_log_entry
+    # ... implementation omitted ...
+  end
+end
