01a94e3f50800f1e67e2687c72c188169a34d952
riazmohamed <railsdevriaz@gmail.com>
2021-09-27 15:41:47 +0100

    add board and squares

--- Lesson5/ttt_oo.rb
+++ Lesson5/ttt_oo.rb
@@ -1,10 +1,23 @@
 class Board
+  INITIAL_MARKER = " "
+
   def initialize
+    @squares = {}#{1 => Square.new(' '), 2 => Square.new(' ')}
+    (1..9).each { |key| @squares[key] = Square.new(INITIAL_MARKER) }
+  end
+
+  def get_square_at(key)
+    @squares[key]
   end
 end
 
 class Square
-  def initialize
+  def initialize(marker)
+    @marker = marker
+  end
+
+  def to_s
+    @marker
   end
 end
 
@@ -17,17 +30,49 @@ class Player
 end
 
 class TTTGame
+  attr_reader :board
+
+  def initialize
+    @board = Board.new
+  end
+
+  def display_welcome_message
+    puts "Welcome to Tic Tac Toe!"
+    puts ""
+  end
+
+  def display_goodbye_message
+    puts "Thank you for playing Tic Tac Toe!"
+  end
+
+  def display_board
+    puts ""
+    puts "     |     |"
+    puts "  #{board.get_square_at(1)}  |     |"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "     |     |"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "     |     |"
+    puts "     |     |"
+    puts ""
+  end
+
   def play
     display_welcome_message
     loop do
       display_board
+      break
       first_player_moves
       break if someone_won? || board_full?
 
       second_player_moves
       break if someone_won? || board_full?
     end
-    display_result
+    #display_result
     display_goodbye_message
   end
 end
