aa9c8e7c94fe5a896ea28d8628d39e923c9ec5f1
riazmohamed <railsdevriaz@gmail.com>
2021-09-27 23:31:17 +0100

    Add Human and Computer move

--- Lesson5/ttt_oo.rb
+++ Lesson5/ttt_oo.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Board
   INITIAL_MARKER = " "
 
@@ -9,9 +11,15 @@ class Board
   def get_square_at(key)
     @squares[key]
   end
+
+  def set_square_at(key, marker)
+    @squares[key].marker = marker
+  end
 end
 
 class Square
+  attr_accessor :marker
+
   def initialize(marker)
     @marker = marker
   end
@@ -22,18 +30,22 @@ class Square
 end
 
 class Player
-  def initialize
-  end
-
-  def mark
+  attr_reader :marker
+  def initialize(marker)
+    @marker = marker
   end
 end
 
 class TTTGame
-  attr_reader :board
+  HUMAN_MARKER = "X"
+  COMPUTER_MARKER = "O"
+
+  attr_reader :board, :human, :computer
 
   def initialize
     @board = Board.new
+    @human = Player.new(HUMAN_MARKER)
+    @computer = Player.new(COMPUTER_MARKER)
   end
 
   def display_welcome_message
@@ -61,16 +73,35 @@ class TTTGame
     puts ""
   end
 
+  def human_moves
+    puts "Choose a square between 1-9: "
+    square = nil
+    loop do
+      square = gets.chomp.to_i
+      break if (1..9).include?(square)
+      puts "Sorry, that's not a valid choice."
+    end
+
+    # binding.pry # => debugging only
+    board.set_square_at(square, human.marker)
+  end
+
+  def computer_moves
+    board.set_square_at((1..9).to_a.sample, computer.marker)
+  end
+
   def play
     display_welcome_message
     loop do
       display_board
-      break
-      first_player_moves
-      break if someone_won? || board_full?
+      human_moves
+      display_board # => debugging only
+      # break if someone_won? || board_full?
 
-      second_player_moves
-      break if someone_won? || board_full?
+      computer_moves
+      display_board
+      # break if someone_won? || board_full?
+      break
     end
     #display_result
     display_goodbye_message
