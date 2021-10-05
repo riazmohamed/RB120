3b8139695dff41318a7e7d650f8a0fc5e1f4909e
riazmohamed <railsdevriaz@gmail.com>
2021-10-05 19:22:32 +0100

    add privacy.rb in excercises

---
+++ Exercises/privacy.rb
@@ -0,0 +1,21 @@
+class Machine
+  def start
+    self.flip_switch(:on)
+  end
+
+  def stop
+    self.flip_switch(:off)
+  end
+
+  private
+  
+  attr_writer :switch
+
+  def flip_switch(desired_state)
+    self.switch = desired_state
+  end
+end
+
+machine = Machine.new
+p machine.start
+p machine.stop
