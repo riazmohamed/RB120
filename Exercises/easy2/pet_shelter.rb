99ac5147d3aeab147511f48e0ea92bb23d9f25f2
riazmohamed <railsdevriaz@gmail.com>
2021-10-19 17:40:17 +0100

    implement shelter.print_adoptions functionality

--- Exercises/easy2/pet_shelter.rb
+++ Exercises/easy2/pet_shelter.rb
@@ -1,11 +1,8 @@
-require 'pry'
-
 class Shelter
   attr_reader :name, :type, :owner_collection
 
   def initialize
     @owner_collection = {}
-    # @pet_collection = {}
   end
 
   def pet_collection(pets_name, pets_type)
@@ -25,21 +22,12 @@ class Shelter
   end
 
   def adopt(owner_object, pet_object)
-    # required methods
     owners_name = owner_object.name
     pets_name = pet_object.name
     pets_type = pet_object.type
 
-    # required format to capture the details
-    # @owner_collection = { owners_name: { pets_type: pets_name } }
-
-    # Create the pets collection
-    # collect all the names of the pets in an array
-
-    # Grouping pets in a hash/array combination
     collection = pet_collection(pets_name, pets_type)
 
-    # Group Owners collection of pets and their types
     if owner_collection.has_key?(owners_name)
 
       if has_the_pet?(pets_type, owners_name)
@@ -58,19 +46,15 @@ class Shelter
   end
 
   def print_adoptions
-    # go through the collection of matching owner and the list of pets he/she owns in the following format for each owner
-
-    # owner_name has adopted the following pets:
-    # a type_of_pet named pet_name
-    # a type_of_pet named pet_name
-    # a type_of_pet named pet_name
-    #
-    # like the following output
-    # B Holmes has adopted the following pets:
-    # a dog named Molly
-    # a parakeet named Sweetie Pie
-    # a dog named Kennedy
-    # a fish named Chester
+    owner_collection.each do |owner, pet_group|
+      puts "#{owner} has adopted the following pets:"
+      pet_group.each do |group|
+        group.each do |type, names|
+          names.each { |name| puts "a #{type} named #{name}" }
+        end
+      end
+      puts ""
+    end
   end
 end
 
@@ -85,7 +69,6 @@ end
 
 class Owner
   attr_reader :name
-  # attr_writer :number_of_pets
 
   def initialize(name)
     @name = name
@@ -121,12 +104,12 @@ shelter.adopt(bholmes, kennedy)
 shelter.adopt(bholmes, sweetie)
 shelter.adopt(bholmes, molly)
 shelter.adopt(bholmes, chester)
-# shelter.print_adoptions
+shelter.print_adoptions
 # puts "#{phanson.name} has #{phanson.number_of_pets} adopted pets."
 # puts "#{bholmes.name} has #{bholmes.number_of_pets} adopted pets."
 
 =begin
-the following is the required output
+the following is the required output for shelter.print_adoptions
 =====================
 P Hanson has adopted the following pets:
 a cat named Butterscotch
@@ -145,4 +128,4 @@ B Holmes has 4 adopted pets.
 =end
 
 # tests
-p shelter.owner_collection
+# p shelter.owner_collection#.each { |key, value| puts key }
